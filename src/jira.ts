bf2ab4ef7d55455722224538756fdf3e77c82fc4
Johan Walles <johan.walles@gmail.com>
2023-02-12 06:48:33 +0100

    Add tests for finding issue ID in subject

--- src/jira.ts
+++ src/jira.ts
@@ -4,6 +4,17 @@ const jiraCapsUrl = vscode.Uri.parse(
   "https://confluence.atlassian.com/adminjiraserver/changing-the-project-key-format-938847081.html"
 );
 
+interface JiraIssueIdPrefix {
+  /** 0 for "JIRA-123: ..." and 1 for "[JIRA-123] ..." */
+  startIndex: number;
+
+  /** At which index does the actual subject line start? */
+  firstIndexAfter: number;
+
+  /** Example: "JIRA-123" */
+  issueId: string;
+}
+
 export default function getJiraDiagnostics(
   doc: vscode.TextDocument
 ): vscode.Diagnostic[] {
@@ -26,6 +37,10 @@ export default function getJiraDiagnostics(
   return returnMe;
 }
 
+function findIssueId(firstLine: string): JiraIssueIdPrefix | undefined {
+  return undefined;
+}
+
 /**
  * Warn if JIRA ticket identifier isn't in ALL CAPS ("dev-1234" should be
  * "DEV-1234")
@@ -34,7 +49,11 @@ function getJiraCapsDiagnostic(firstLine: string): vscode.Diagnostic[] {
   return [];
 }
 
+// Exports for testing
+//
+// Ref: https://stackoverflow.com/a/65422568/473672
 export const _private = {
   jiraCapsUrl,
   getJiraCapsDiagnostic,
+  findIssueId,
 };
--- src/test/suite/jira.test.ts
+++ src/test/suite/jira.test.ts
@@ -5,6 +5,22 @@ import * as extension from "../../extension";
 import * as jira from "../../jira";
 
 suite("Git Commit Message Plus", () => {
+  test("Find issue ID", () => {
+    assert.deepStrictEqual(jira._private.findIssueId("[Jira-123] Hello"), {
+      startIndex: 1,
+      firstIndexAfter: 11,
+      issueId: "Jira-123",
+    });
+
+    assert.deepStrictEqual(jira._private.findIssueId("jira-123: Hello"), {
+      startIndex: 0,
+      firstIndexAfter: 10,
+      issueId: "jira-123",
+    });
+
+    assert.equal(jira._private.findIssueId("Hello"), undefined);
+  });
+
   test("[Jira-123] JIRA issue ID capitalization", () => {
     const expected = extension._private.createDiagnostic(
       0,
@@ -19,7 +35,7 @@ suite("Git Commit Message Plus", () => {
     );
 
     assert.deepStrictEqual(
-      extension._private.getFirstLineCapsDiagnostic("[Jira-123] Hello"),
+      jira._private.getJiraCapsDiagnostic("[Jira-123] Hello"),
       [expected]
     );
   });
@@ -38,7 +54,7 @@ suite("Git Commit Message Plus", () => {
     );
 
     assert.deepStrictEqual(
-      extension._private.getFirstLineCapsDiagnostic("jira-123: Hello"),
+      jira._private.getJiraCapsDiagnostic("jira-123: Hello"),
       [expected]
     );
   });
