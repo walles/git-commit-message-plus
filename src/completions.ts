ccaa0e0ff229e6aceabe0f8fc67cb855a7328fc2
Johan Walles <johan.walles@gmail.com>
2023-02-13 19:50:28 +0100

    Let's go with not-inline completions

--- src/completions.ts
+++ src/completions.ts
@@ -1,33 +1,30 @@
 import {
-  CancellationToken,
-  InlineCompletionContext,
-  InlineCompletionItem,
-  InlineCompletionItemProvider,
-  InlineCompletionList,
+  CompletionItemProvider,
   Position,
   ProviderResult,
   TextDocument,
+  CompletionItem,
+  CompletionList,
 } from "vscode";
 
 export default class GitCommitCompletionsProvider
-  implements InlineCompletionItemProvider
+  implements CompletionItemProvider
 {
-  provideInlineCompletionItems(
+  provideCompletionItems(
     document: TextDocument,
-    position: Position,
-    context: InlineCompletionContext,
-    token: CancellationToken
-  ): ProviderResult<InlineCompletionItem[] | InlineCompletionList> {
+    position: Position
+  ): ProviderResult<CompletionItem[] | CompletionList<CompletionItem>> {
     if (position.line != 0) {
       return [];
     }
     if (position.character != 0) {
       return [];
     }
-    return [
-      {
-        insertText: "johan",
-      },
-    ];
+    return [new CompletionItem("johangris"), new CompletionItem("kalasgris")];
+  }
+
+  resolveCompletionItem?(item: CompletionItem): ProviderResult<CompletionItem> {
+    // This method intentionally left blank. Until further notice.
+    return item;
   }
 }
