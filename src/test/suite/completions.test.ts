674082e2c4d8f2c835f145bf4e881a035f9ba77f
Johan Walles <johan.walles@gmail.com>
2023-02-13 21:13:13 +0100

    Add a bunch of completion tests
    The ones I could come up with for now.

--- src/test/suite/completions.test.ts
+++ src/test/suite/completions.test.ts
@@ -3,24 +3,54 @@ import * as vscode from "vscode";
 import { createTextDocument } from "./common";
 import * as completions from "../../completions";
 
+const BRANCH = "jira-123";
+
 suite("JIRA Issue ID Completions From Branch Name", () => {
-  const BRANCH = "jira-123";
-
   test("Line: Empty", async () => {
-    const doc = await createTextDocument([""]);
-    const pos = new vscode.Position(0, 0);
-    const actual = completions._private.getBranchIssueCompletion(
-      doc,
-      pos,
-      BRANCH
-    );
-
-    assert.deepEqual(actual, [
+    assert.deepEqual(await getCompletion("", 0), [
       completion("JIRA-123: ", 0, 0),
       completion("[JIRA-123] ", 0, 0),
     ]);
   });
+
+  test("Line: [", async () => {
+    assert.deepEqual(await getCompletion("[", 1), [
+      completion("[JIRA-123] ", 0, 0),
+    ]);
+  });
+
+  test("Line: [|]", async () => {
+    assert.deepEqual(await getCompletion("[]", 1), [
+      completion("[JIRA-123] ", 0, 1),
+    ]);
+  });
+
+  test("Line: j", async () => {
+    assert.deepEqual(await getCompletion("j", 1), [
+      completion("JIRA-123: ", 0, 1),
+    ]);
+  });
+
+  test("Line: k", async () => {
+    assert.deepEqual(await getCompletion("k", 1), []);
+  });
+
+  test("Line: [apa-321]", async () => {
+    // Wrong ticket number, offer to fix it
+    assert.deepEqual(await getCompletion("apa-321: ", 9), [
+      completion("JIRA-123: ", 0, 1),
+    ]);
+  });
 });
+
+async function getCompletion(
+  firstLine: string,
+  position: number
+): Promise<vscode.CompletionItem[]> {
+  const doc = await createTextDocument([firstLine]);
+  const pos = new vscode.Position(0, position);
+  return completions._private.getBranchIssueCompletion(doc, pos, BRANCH);
+}
 
 function completion(
   label: string,
