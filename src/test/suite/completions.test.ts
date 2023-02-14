32a92f7adf71b99d5d8e507658042d752ec387d4
Johan Walles <johan.walles@gmail.com>
2023-02-14 22:22:58 +0100

    Focus tests on completions and : only

--- src/test/suite/completions.test.ts
+++ src/test/suite/completions.test.ts
@@ -5,82 +5,35 @@ import * as completions from "../../completions";
 
 const BRANCH = "jira-123";
 
+/**
+ * @param line Must contain a `|` to mark the cursor position
+ */
+function testLine(line: string, expectedCompletions: vscode.CompletionItem[]) {
+  test("Line: " + line, async () => {
+    const cursorPosition = line.indexOf("|");
+    assert.ok(
+      cursorPosition > 0,
+      "Must put a | in the line to mark the cursor position"
+    );
+
+    const withoutCursorMarker = line.replace("|", "");
+
+    assert.deepEqual(
+      await getCompletion(withoutCursorMarker, cursorPosition),
+      expectedCompletions
+    );
+  });
+}
+
 suite("JIRA Issue ID Completions From Branch Name", () => {
-  test("Line: Empty", async () => {
-    assert.deepEqual(await getCompletion("", 0), [
-      completions.completion("JIRA-123: ", 0, 0),
-      completions.completion("[JIRA-123] ", 0, 0),
-    ]);
-  });
+  testLine("|", [completions.completion("JIRA-123: ", 0, 0)]);
+  testLine("jir|", [completions.completion("JIRA-123: ", 0, 2)]);
+  testLine("JIR|", [completions.completion("JIRA-123: ", 0, 2)]);
+  testLine("jira-123|", [completions.completion("JIRA-123: ", 0, 7)]);
+  testLine("JIRA-123|", []);
+  testLine("jira-123:|", []);
 
-  test("Line: [", async () => {
-    assert.deepEqual(await getCompletion("[", 1), [
-      completions.completion("[JIRA-123] ", 0, 0),
-    ]);
-  });
-
-  test("Line: [|]", async () => {
-    assert.deepEqual(await getCompletion("[]", 1), [
-      completions.completion("[JIRA-123] ", 0, 1),
-    ]);
-  });
-
-  test("Line: j", async () => {
-    assert.deepEqual(await getCompletion("j", 1), [
-      completions.completion("JIRA-123: ", 0, 1),
-    ]);
-  });
-
-  test("Line: j| Hello", async () => {
-    // User went back to retroactively add a JIRA ticket number at the start
-    assert.deepEqual(await getCompletion("j", 1), [
-      completions.completion("JIRA-123: ", 0, 2),
-    ]);
-  });
-
-  test("Line: k", async () => {
-    assert.deepEqual(await getCompletion("k", 1), []);
-  });
-
-  test("Line: [apa-321]", async () => {
-    // Wrong ticket number in range, offer to fix it
-    assert.deepEqual(await getCompletion("[apa-321] ", 9), [
-      completions.completion("[JIRA-123] ", 0, 9),
-    ]);
-  });
-
-  test("Line: apa-321: ", async () => {
-    // Wrong ticket number in range, offer to fix it
-    assert.deepEqual(await getCompletion("apa-321: ", 9), [
-      completions.completion("JIRA-123: ", 0, 9),
-    ]);
-  });
-
-  test("Line: [apa-321] Hello|", async () => {
-    // Wrong ticket number out of range, don't touch it
-    assert.deepEqual(await getCompletion("[apa-321] Hello", 15), []);
-  });
-
-  test("Line: [apa-321] |Hello", async () => {
-    // Wrong ticket number in range, don't touch it
-    assert.deepEqual(await getCompletion("[apa-321] Hello", 10), [
-      completions.completion("[JIRA-123] ", 0, 9),
-    ]);
-  });
-
-  test("Line: apa-3|21: Hello", async () => {
-    // Replace the right part
-    assert.deepEqual(await getCompletion("apa-321: Hello", 5), [
-      completions.completion("JIRA-123: ", 0, 9),
-    ]);
-  });
-
-  test("Line: apa-54321: ", async () => {
-    // Wrong ticket number in range, offer to fix it, which will make it shorter
-    assert.deepEqual(await getCompletion("apa-54321: ", 11), [
-      completions.completion("JIRA-123: ", 0, 11),
-    ]);
-  });
+  // FIXME: Add tests for bracketed issue IDs as well
 });
 
 async function getCompletion(
