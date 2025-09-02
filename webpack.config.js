f4ea5df9e27c041c29a282aefa211850e208ac73
Johan Walles <johan.walles@gmail.com>
2025-09-02 20:15:37 +0200

    Pass compiling and sort of linting
    (node:88768) [MODULE_TYPELESS_PACKAGE_JSON] Warning: Module type of file:///Users/johan/src/git-commit-message-plus/eslint.config.js?mtime=1756830268485 is not specified and it doesn't parse as CommonJS.
    Reparsing as ES module because module syntax was detected. This incurs a performance overhead.
    To eliminate this warning, add "type": "module" to /Users/johan/src/git-commit-message-plus/package.json.
    (Use `node --trace-warnings ...` to show where the warning was created)

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ const config = {
       // Webpack 5 no longer polyfills Node.js core modules automatically.
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
+      path: false,
+      child_process: false,
+      util: false
+      // add other Node.js modules as needed
     }
   },
   module: {
